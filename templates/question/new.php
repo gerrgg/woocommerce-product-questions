89963a9f3cabe72b84b9e7ac79d95e867df2f7dd
=Greg Bastianelli <=gregbast1994@gmail.com>
2019-09-03 13:44:50 -0400

    change new question form

--- templates/question/new.php
+++ templates/question/new.php
@@ -3,7 +3,12 @@
 <form method="POST" action="<?php echo admin_url( 'admin-post.php' ) ?>" >
     <h5 class="font-weight-bold text-center"> Don't see the answer you are looking for?</h5>
     <div class="d-flex d-md-block text-center ">
-        <input type="email" name="email" placeholder="user@example.com" /> <br>
+
+        <?php if( ! is_user_logged_in() ) :  // add email input if not logged in. ?>
+            <input type="email" name="email" placeholder="user@example.com" /> <br>
+        <?php else : ?>
+            <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>" />
+        <?php endif; ?>
         <button id="ask-question" type="submit" class="btn btn-dark">Ask</button>
     </div>
     <input type="hidden" name="question" value="<?php echo get_query_var('question') ?>" />
